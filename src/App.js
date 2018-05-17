195a0f349af00ebe4fdf9aaca992d6d07ba0bc1c
Ricky Do <ricky.do@dps.texas.gov>
2018-05-17 11:54:49 -0500

    update for play again

--- src/App.js
+++ src/App.js
@@ -3,10 +3,20 @@ import './App.css';
 import Game from './Game';
 
 class App extends Component {
+  state = {
+    gameId: 1,
+  };
+
   render() {
     return (
       <div className="App">
-        <Game/>
+        <Game key={this.state.gameId}/>
+        {/* use keys to replace game component for play again */}
+        <button onClick={ () => {
+          this.setState( (prevState) => ({
+            gameId: prevState.gameId + 1
+          }))
+        }}>Play Again</button>
       </div>
     );
   }
--- src/Game.js
+++ src/Game.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import sampleSize from 'lodash.samplesize';
 
 import Key from './Key';
 
@@ -13,48 +14,67 @@ const targetColors = {
 class Game extends Component {
   state = {
     gameStatus: 'playing', // 'won', 'lost'
-    selectedKeys: [],
+    selectedKeys: []
   }
 
   // not part of the state, just onload
-  challengeKeys = Array.from({ length: 6 }).map(() => randomNumberBetween(2, 10));
-  target = this.challengeKeys.slice(0, 4).reduce((curr, acc) => curr + acc, 0);
+  challengeKeys = Array.from({length: 6}).map(() => randomNumberBetween(2, 10));
+  target = sampleSize(this.challengeKeys, 4).reduce((acc, curr) => curr + acc, 0);
 
   selectKey = (keyId) => {
     this.setState((prevState) => {
+      if (prevState.gameStatus !== 'playing') {
+        return null;
+      }
       // setState is asynchronous
       // using updater function guarantees you dont run into race conditions
       // prevState.selectedKeys
+      const newSelectedKeys = [
+        ...prevState.selectedKeys,
+        keyId
+      ]
       return {
-        selectedKeys: [...prevState.selectedKeys, keyId] // js spread array: copies first array and adds in 2nd param
+        selectedKeys: newSelectedKeys, // js spread array: copies first array and adds in 2nd param
+        gameStatus: this.calcGameStatus(newSelectedKeys)
       }
     });
   };
 
-  render() {
+  calcGameStatus = (newSelectedKeys) => {
+    if (newSelectedKeys.length !== 4) {
+      return 'playing';
+    }
+
+    const sumSelected = newSelectedKeys.reduce((acc, curr) => {
+      acc + this.challengeKeys[curr],
+      0
+    });
+
+    console.log("sumSelected", sumSelected);
     return (
-      <div className="game">
-        <div className="help">Pick 4 numbers that sum to the target in 15 seconds</div>
-        <div className="target" style={{backgroundColor: targetColors[this.state.gameStatus]}}>{this.target}</div>
-        <div className="challenge-numbers">
-          {this.challengeKeys.map((value, index) => (
-            <Key
-              key={index}
-              value={value}
-              isClickable={this.state.selectedKeys.indexOf(index) === -1}
-              id={index}
-              onClickAction={this.selectKey} // pass Key component fn
-         />))}
-                       {/* // if you do anything like reordering or removing elements
+      sumSelected === this.target
+      ? 'won'
+      : 'lost')
+  }
+
+  render() {
+    return (<div className="game">
+      <div className="help">Pick 4 numbers that sum to the target in 15 seconds</div>
+      <div className="target" style={{
+          backgroundColor: targetColors[this.state.gameStatus]
+        }}>{this.target}</div>
+      <div className="challenge-numbers">
+        {
+          this.challengeKeys.map((value, index) => (<Key key={index} value={value} isClickable={this.state.selectedKeys.indexOf(index) === -1} id={index} onClickAction={this.selectKey}
+            // pass Key component fn
+/>))
+        }
+        {/* // if you do anything like reordering or removing elements
                        // do not use index as a key
-                       // the key component needs to just know if it's selected or not */}
+                       // the key component needs to just know if it's selected or not */
+        }
         </div>
-        <div className="footer">
-          <div className="timer-value">15</div>
-          <button>Start</button>
-        </div>
-      </div>
-    );
+    </div>);
   }
 }
 
